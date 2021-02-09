72e6f9b5c80d5c84f9597a566b0fa56f3bd0430d
uli <up@plabst.org>
2021-02-09 13:21:25 +0000

    hash in list

--- build.js
+++ build.js
@@ -1,12 +1,31 @@
 const fs = require("fs");
 const path = require("path");
-let entries = fs.readdirSync("./snippets");
 
+function hash(str) {
+  var hash = 0;
+  if (str.length == 0) {
+      return hash;
+  }
+  for (let i = 0; i < str.length; i++) {
+      var char = str.charCodeAt(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+let entries = fs.readdirSync("./snippets");
 let list = [];
 for(let e of entries)
 {
-  fs.copyFileSync(path.join("./snippets", e), path.join("./docs", e));
-  list.push(e);
+  let src = path.join("./snippets", e);
+  let content = fs.readFileSync(src).toString();
+  let h = hash(content);
+  fs.copyFileSync(src, path.join("./docs", e));
+  list.push({
+    name: e,
+    hash: h
+  });
 }
 
 fs.writeFileSync("./docs/list.json", JSON.stringify(list));
